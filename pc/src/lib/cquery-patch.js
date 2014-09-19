b731f1390954e260bbd4f60b549c6eb719b13371
lin_m <lin_m@ctrip.com>
2014-09-19 11:27:50 +0800

    first implementation commit for cquery-patch.js added dynamic method: - $.fn.show - $.fn.hide

--- pc/src/lib/cquery-patch.js
+++ pc/src/lib/cquery-patch.js
@@ -4,7 +4,31 @@
  * @descriptions 对cQuery框架一个API扩展，以及API的补丁
  * @warn 写的时候一定要考虑是会影响到框架自身，禁止重载框架方法，给fn添加方法前一定要判断，比如 !$.fn.show && ($.fn.show=...)
  */
-(function ($, undefined){
 
+ //静态方法
+ (function ($, undefined){
 
 })(cQuery);
+
+//动态方法
+ (function ($, fn, undefined){
+    methods =  {};
+    methods.show = function (option) {
+        this.each(function (item) {
+            item.css('display', option || 'block');
+        });
+    };
+    methods.hide = function () {
+        this.each(function (item) {
+            item.css('display', 'none');
+        });
+    };
+    
+    (function exportDynamicMethods (methods) {
+        var name;
+        for (name in methods) if (methods.hasOwnProperty(name)) {
+            fn[name] === undefined && (fn[name] = methods[name]);
+        }
+    } (methods));
+})(cQuery, cQuery.fn);
+
